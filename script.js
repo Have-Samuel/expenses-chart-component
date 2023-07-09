7b4fd644641f521f05837728c3749583e6f697ad
Have Samuel <samiehave9@gmail.com>
2023-07-09 22:09:58 +0300

    Creating the whole chart data

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const myChart = document.querySelector('#myChart').getContext('2d');
 
-const barChart = new CharacterData(myChart);
+const barChart = new CharacterData(myChart, {
+  type: 'bar',
+  data: {
+    days: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+    datasets: [{
+      label: 'Amount',
+      data: []
+    }],
+  },
+});
