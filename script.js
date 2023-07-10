ed595e29a43b68f53e222779ebec4a115c09ac7b
Have Samuel <samiehave9@gmail.com>
2023-07-11 00:14:31 +0300

    Exploiting Chart.js

--- script.js
+++ script.js
@@ -1,47 +1,62 @@
-const myChart = document.querySelector('#myChart');
+// const myChart = document.querySelector('#myChart');
 
-    new Chart(myChart, {
+// // Global Options
+// // myChart.defaults.global.defaultFontSize = 16;
+
+// new Chart(myChart, {
+//   type: 'bar',
+//   data: {
+//     labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+//     datasets: [{
+//       label: 'Amount of Cash',
+//       data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+//       // borderWidth: 0.5,
+//       borderRadius: 5,
+//       backgroundColor: [
+//         '#ec755d',
+//         '#ec755d',
+//         '#76B5BC',
+//         '#ec755d',
+//         '#ec755d',
+//         '#ec755d',
+//         '#ec755d',
+//       ],
+//     }]
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: 'Amounts',
+//       fontSize: 20,
+//       color: 'black'
+//     },
+//     legend: {
+//       position: 'right',
+//   },
+//   }
+// });
+
+const ctx = document.querySelector('#myChart');
+
+  new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+      labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun' ],
       datasets: [{
-        label: 'Amount of Cash',
-        data: [ 17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
-        borderWidth: 0.5
-    }],
-      // backgroundColor: 'red',
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.6)',
-        'rgba(255, 99, 132, 0.6)',
-      ],
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3,4],
+        borderWidth: 1,
+      }],
     },
     options: {
-
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      title: {
+        text: 'Amount',
+        color: 'red',
+      }
     },
-});
-
-// const ctx = document.querySelector('#myChart');
-
-//   new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun', ],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-//   });
+  });
