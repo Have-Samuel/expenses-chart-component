5628fb843c21e6b3608f7e2d249acaf577a1db45
Have Samuel <samiehave9@gmail.com>
2023-07-11 02:38:02 +0300

    Removing the Ticksand background

--- script.js
+++ script.js
@@ -5,40 +5,46 @@
 // Chart.defaults.fontColor = '#777';
 // Chart.defaults.fontSize = 16;
 // Chart.defaults.fontFamily = 'Helvetica Neue';
-// Chart.defaults.fontStyle = 'bold';
-// Chart.defaults.borderColor = '#fff';
+Chart.defaults.fontStyle = 'bold';
+Chart.defaults.borderColor = '#fff';
 
-// new Chart(myChart, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
-//     datasets: [{
-//       label: 'Amount of Cash',
-//       data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
-//       // borderWidth: 0.5,
-//       borderRadius: 5,
-//       backgroundColor: [
-//         '#ec755d',
-//         '#ec755d',
-//         '#76B5BC',
-//         '#ec755d',
-//         '#ec755d',
-//         '#ec755d',
-//         '#ec755d',
-//       ],
-//     }],
-//   },
-//   options: {
-//     scales: {
-//       myScale: {
-//         type: 'logarithmic',
-//         position: 'left', // `axis` is determined by the position as `'y'`
-//       }
-//     }
-//   },
-// });
+new Chart(myChart, {
+  type: 'bar',
+  data: {
+    labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+    datasets: [{
+      label: 'Amount of Cash',
+      data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+      // borderWidth: 0.5,
+      borderRadius: 5,
+      backgroundColor: [
+        '#ec755d',
+        '#ec755d',
+        '#76B5BC',
+        '#ec755d',
+        '#ec755d',
+        '#ec755d',
+        '#ec755d',
+      ],
+    }],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          display: false,
+        },
+        grid: {
+          drawTicks: false,
+          drawBorder: false,
+        },
+      },
+    }
+  },
+});
 
-const ctx = document.querySelector('#myChart');
+// const ctx = document.querySelector('#myChart');
 
 // new Chart(ctx, {
 //   type: 'bar',
@@ -63,21 +69,36 @@ const ctx = document.querySelector('#myChart');
 //   },
 // });
 
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    datasets: [{
-      label: 'First dataset',
-      data: [0, 20, 40, 50],
-    }],
-    labels: ['January', 'February', 'March', 'April'],
-  },
-  options: {
-    scales: {
-      y: {
-        suggestedMin: 50,
-        suggestedMax: 100,
-      },
-    },
-  },
-});gi
+// new Chart(ctx, {
+//   type: 'line',
+//   data: {
+//     datasets: [{
+//       data: [10, 20, 30, 40, 50, 60],
+//       label: 'Left dataset',
+
+//       // This binds the dataset to the left y axis
+//       yAxisID: 'left-y-axis',
+//     }, {
+//       data: [1, 2, 3, 4, 5, 6],
+//       label: 'Right dataset',
+
+//       // This binds the dataset to the right y axis
+//       yAxisID: 'right-y-axis',
+//     }],
+//     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+//   },
+//   options: {
+//     scales: {
+//         xAxes: [{
+//             gridLines: {
+//                 drawOnChartArea: false,
+//             }
+//         }],
+//         yAxes: [{
+//             gridLines: {
+//                 drawOnChartArea: false,
+//             }
+//         }],
+//     },
+//   }
+// });
